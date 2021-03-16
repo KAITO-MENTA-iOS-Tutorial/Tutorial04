3ee68600408f5c9c3cc375462bce10967990c438
Kaito Hattori <kaichan394@gmail.com>
2021-03-16 09:37:42 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Tutorial04
+
+ボタンを押して、TextFieldに入力した文字をLabelに表示するアプリ
+
+
+## スクリーンショット
+
+![スクリーンショット](screenshot.png)
--- Tutorial04/ViewController.swift
+++ Tutorial04/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hogeLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onClickButton(_ sender: Any) {
+        let currentText = textField.text
+        self.hogeLabel.text = currentText
+    }
 }
 
